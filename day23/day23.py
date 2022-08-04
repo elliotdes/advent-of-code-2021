76e9dbfa2fe2ed779203719c2d4eb4144707556e
Elliot Simons <elliotdesimons@gmail.com>
2022-08-04 13:57:55 +0100

    WIP: uncompleted puzzles

---
+++ day23/day23.py
@@ -0,0 +1,29 @@
+def parse_input(input_file: str):
+    ...
+
+
+def part_1():
+    ...
+
+
+def part_2():
+    ...
+
+
+# Tests
+
+
+def test_parse_input():
+    ...
+
+
+def test_part_1():
+    ...
+
+
+def test_part_2():
+    ...
+
+
+if __name__ == "__main__":
+    pass
